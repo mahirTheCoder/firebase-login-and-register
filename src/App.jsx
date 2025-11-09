f52e2b84c8134f0b2234a171cc5732ee33adafb1
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-11-09 18:05:19 +0600

    note

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import { createBrowserRouter, createRoutesFromElements } from 'react-router'
 import Register from './Components/Register'
 import Login from './Components/Login'
 import app from './Firbase.config'
+import Note from './Components/Note'
 
 const App = () => {
 const myRoute = createBrowserRouter(createRoutesFromElements(
 <Route>
 <Route path='/' element={<Register/>}/>
 <Route path='/Login' element={<Login/>}/>
+<Route path='/Note' element={<Note/>}/>
 </Route>  
 ))
 
